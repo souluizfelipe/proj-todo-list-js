f38ea1aadba730cd6881834438a0ed98f7bc1638
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-07-22 13:43:00 -0300

    create event to add new task on the to do list and remove li items from html code

--- js/main.js
+++ js/main.js
@@ -7,18 +7,46 @@ const Main = {
   
   cacheSelectors: function() {
     this.$checkButtons = document.querySelectorAll('.check');
+    this.$inputText = document.querySelector('#inputText');
+    this.$list = document.querySelector('#list');
   },
 
   bindEvents: function() {
+    
     this.$checkButtons.forEach(button => {
-      button.addEventListener('click', this.Events.checkButton_click  );
-   })
+      button.addEventListener('click', this.Events.checkButton_click);
+   });
+   
+  //  this.$inputText.addEventListener('keypress', self.Events.inputText_keypress.bind(this));
+   this.$inputText.onkeypress = this.Events.inputText_keypress.bind(this)
   },
   
   Events: {
     checkButton_click: function(e) {
       e.target.parentElement.classList.toggle('done');
-    }
+    },
+
+    inputText_keypress: function(e) {
+      const key = e.key;
+      const value = e.target.value;
+
+      if(key === 'Enter') {
+        this.$list.innerHTML += `
+          <li>
+            <div class="check"></div>
+            <label class="task">
+              ${value}
+            </label>
+            <button class="remove"></button>
+          </li> 
+        `
+        e.target.value = "";
+        
+        this.cacheSelectors();
+        this.bindEvents();
+      };
+    },
+
   },
 };
 
